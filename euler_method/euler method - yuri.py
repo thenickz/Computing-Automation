837ff1d06ea6f926d4ff647ab6e3eaf3f47728d0
thenickz <nicklopes2098@gmail.com>
2022-03-06 00:09:06 -0300

    inicial

---
+++ euler_method/euler method - yuri.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+def f(x):
+    return x**2 + 2*x*(x+1)
+
+
+def func(x, y):
+    return y + x
+
+
+def Euler_Method(func, y0, x0, b, n):
+    
+    h = (b - x0)/n
+    
+    x = x0
+    yi = y0 + h*func(x,y0)
+    #print("Para a iteração " + str(0) + ", resulta em "+ str(yi))
+    xi = x0+h
+    
+    for i in range(1,n):
+        xi = xi + h
+        yi = yi + h*(func(xi,yi))
+        #print("Para a iteração " + str(i+1) + ", resulta em "+ str(yi))
+
+    return yi
+
